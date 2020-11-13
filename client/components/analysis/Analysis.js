a061bcfe24f904f2c6fb63cd342c67d1c318ce77
Lucas <l.erlacher@icloud.com>
2020-11-13 19:14:01 +0100

    implemented pie chart

--- client/components/analysis/Analysis.js
+++ client/components/analysis/Analysis.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Text, View, FlatList, Button, Picker, Dimensions, ScrollView } from 'react-native';
+import { Text, View, FlatList, Button, Picker, Dimensions, ScrollView, Alert } from 'react-native';
 import {
   LineChart,
   BarChart,
@@ -11,164 +11,242 @@ import {
 
 import config from './../../config';
 const { getLabels } = config.helperFunctions;
+const screenWidth = Dimensions.get("window").width;
+
+import colors from './../../colors';
+console.log('colors-->', colors[1]);
+
+
+const chartConfig = {
+  backgroundGradientFrom: "#1E2923",
+  backgroundGradientFromOpacity: 0,
+  backgroundGradientTo: "#08130D",
+  backgroundGradientToOpacity: 0.5,
+  color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+  strokeWidth: 2, // optional, default 3
+  barPercentage: 0.5,
+  useShadowColorFromDataset: false // optional
+};
+
+const chartTypes = [
+  'none',
+  'LineChart',
+  'BarChart',
+  'PieChart',
+  'ProgressChart',
+  'ContributionGraph',
+  'StackedBarChart'
+];
+
+
+
 
 const Analysis = ({ entries }) => {
   const [firstFilter, setFirstFilter] = useState(getLabels(entries[0])[0]);
   const [secondFilter, setSecondFilter] = useState('');
   const [thirdFilter, setThirdFilter] = useState('');
   const [selectedAnalysys, setSelectedAnalysys] = useState(getLabels(entries[0])[0]);
+  const [selectedChart, setSelectedChart] = useState('');
   const [result, setResult] = useState('');
   const [analysis, setAnalysis] = useState(false);
+  const [chartShow, setChartShow] = useState(false);
 
-  const renderAnalysisResult = (var1, var2, var3) => {
+
+
+  const resCleaner = (str) => {
+    const arr = str.split('');
+    const percentageIndex = arr.findIndex((el) => el === '%');
+    return Number(arr.slice(0, percentageIndex).join(''));
+  };
+
+
+
+  const percentageConverter = (el, tot) => {
+    const res = Number(((Number(el) / tot) * 100).toFixed(2));
+    return res;
+  };
+
+  const randomColorIndexGenerator = () => {
+    // const index = Math.round(Math.random() * colors.length);
+    const index = Math.floor(Math.random() * Math.floor(colors[1].length));
+    console.log('index-->', index);
+    return index;
+
+  };
+
+  const dataGraphCreator = (arr, item) => {
+    const tot = analysisTypes.sum(arr, 'amount');
+    console.log('tot-->', tot);
+    const commoneach = [];
+    arr.forEach((el) => {
+      commoneach.push(Number(((Number(el.amount) / tot) * 100).toFixed(2)));
+    });
+
+
+    let res = [];
+    for (let i = 0; i < arr.length; i++) {
+      let newObj = {
+        name: `% ${arr[i].item}`,
+        population: percentageConverter(arr[i].amount, tot),
+        color: colors[1][randomColorIndexGenerator()],
+        legendFontColor: "#7F7F7F",
+        legendFontSize: 15
+      };
+      res.push(newObj);
+    }
+    console.log('res-->', res);
+    return res;
+  };
+
+  const hardCoded = [
+    {
+      name: "Seoul",
+      population: 21500000,
+      color: "rgba(131, 167, 234, 1)",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15
+    },
+    {
+      name: "Toronto",
+      population: 2800000,
+      color: "#F00",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15
+    },
+    {
+      name: "Beijing",
+      population: 527612,
+      color: "red",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15
+    },
+    {
+      name: "New York",
+      population: 8538000,
+      color: "#ffffff",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15
+    },
+    {
+      name: "Moscow",
+      population: 11920000,
+      color: "rgb(0, 0, 255)",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15
+    }
+  ];
+
+  const showPieChart = (data) => {
+    const dataGraph = dataGraphCreator(data);
+    return (
+      <PieChart
+        data={dataGraph}
+        width={screenWidth}
+        height={220}
+        chartConfig={chartConfig}
+        accessor="population"
+        backgroundColor="transparent"
+        paddingLeft="15"
+        absolute
+      />
+    );
+  };
+
+  const renderAnalysisResult = (...args) => {
+    const firstFil = args[0];
+    const secFil = args[1];
+    const thirdFil = args[2];
+    const selAnalysis = args[3];
+    const result = typeof args[4] === string ? resCleaner(result) : args[4];
+    const string = `The result of the ${selAnalysis} of ${secFil} - ${thirdFil} is: ${result}`;
+
+    const chart = (<View>
+      {/* <Text>Bezier Line Chart</Text> */}
+      <Text>{string}</Text>
+
+    </View>
+    );
     return <>
-      <Text>{var1} -- {var2} -- {var3}</Text>
-      <View>
-        <Text>Bezier Line Chart</Text>
-        <BarChart
-          data={{
-            labels: [var2, 'test'],
-            datasets: [
-              {
-                data: [
-                  var3,
-                  50,
-                ]
-              }
-            ]
-          }}
-          width={Dimensions.get("window").width} // from react-native
-          height={220}
-          yAxisLabel="$"
-          yAxisSuffix="k"
-          yAxisInterval={1} // optional, defaults to 1
-          chartConfig={{
-            backgroundColor: "#e26a00",
-            backgroundGradientFrom: "#fb8c00",
-            backgroundGradientTo: "#ffa726",
-            decimalPlaces: 2, // optional, defaults to 2dp
-            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-            labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-            style: {
-              borderRadius: 16
-            },
-            propsForDots: {
-              r: "6",
-              strokeWidth: "2",
-              stroke: "#ffa726"
-            }
-          }}
-          bezier
-          style={{
-            marginVertical: 8,
-            borderRadius: 16
-          }}
-        />
-      </View>
+      { selAnalysis === 'sum' ? <Text>{string}</Text> : chart}
     </>;
   };
 
   const analysisTypes = {
-    horizontal: () => {
-
-    },
-    trend: () => {
-
-    },
-    commonsize: () => {
-
-    },
-    totalsum: (arr, firstFil, secFil, thirdFil) => {
-      console.log('firstFil-->', firstFil);
-      console.log('arr-->', arr);
-      console.log('thirdFil-->', thirdFil);
-
+    sum: (arr, firstFil, secFil, thirdFil) => {
       const filteredList = filterBySub(arr, secFil, thirdFil);
-      console.log('filteredList-->', filteredList);
       if (filteredList.length === 1) {
         return filteredList[0][firstFil];
       } else {
         return filteredList.reduce((pv, cv) => {
-          console.log('INSIDE 2ND REDUCE-->');
-          console.log('pv-->', pv);
-          console.log('cv-->', cv);
-          console.log('pv[firstFil]-->', pv[firstFil]);
-          console.log('cv[firstFil-->', cv[firstFil]);
-
-          // const res = { res: Number(pv[firstFil]) + Number(cv[firstFil]) };
-          // return res.res;
           return pv + Number(cv[firstFil]);
-
-          // return Number(pv[firstFil]) + Number(cv[firstFil]);
         }, 0);
       }
     },
-    percentageovertot: (filter, arr, selFil, selSubFil) => {
-      // console.log('filter-->', filter);
-      // console.log('arr-->', arr);
-      // console.log('selFil-->', selFil);
-      // console.log('selSubFil-->', selSubFil);
-      // const tot
+    commonsize: (...args) => {
+      const arr = args[0];
+      const firstFil = args[1];
+      const secFil = args[2];
+      const thirdFil = args[3];
+      const totalExp = analysisTypes.sum(arr, firstFil);
+      const filteredTotalExp = analysisTypes.sum(arr, firstFil, secFil, thirdFil);
+      const res = `${((filteredTotalExp / totalExp) * 100).toFixed(2)}%`;
+      return res;
     },
-    sumcategor: () => {
 
-    },
+    // percentageovertot: (filter, arr, selFil, selSubFil) => {
+    // },
+    // sumcategor: () => {
+    // },
+    // horizontal: () => {
+    // },
+    // trend: () => {
+    // },
+    // commonsize: () => {
+    // },
   };
 
-  const handleOutput = (arr, firstFil, secFil, thirdFil, selAnalys) => {
-    console.log('selAnalys-->', selAnalys);
-    console.log('secFil-->', secFil);
-    console.log('thirdFil-->', thirdFil);
-    const output = analysisTypes[selAnalys](arr, firstFil, secFil, thirdFil);
-    // console.log('output-->', output);
-    setResult(output);
+  const handleOutput = (...args) => {
+    const arr = args[0];
+    const firstFil = args[1];
+    const secFil = args[2];
+    const thirdFil = args[3];
+    const selAnalys = args[4];
+    const selGraph = args[5];
+    if (selGraph === 'none') {
+      if (firstFil === 'none' || selAnalys === 'none') {
+        Alert.alert('Plese select at least item second last or graph');
+        setAnalysis(false);
+      } else {
+        const output = analysisTypes[selAnalys](arr, firstFil, secFil, thirdFil);
+        setResult(output);
+      }
+    } else {
+      setChartShow(true);
+      setSelectedChart(selGraph);
+    }
   };
 
   const filterBySub = (arr, secfil, thirdfil) => {
-    console.log('INSIDE FILTERBYSUB-->');
-    console.log('arr-->', arr);
-    console.log('secfil-->', secfil);
-    console.log('thirdfil-->', thirdfil);
-    // if (thirdfil === 'all') {
-    //   return arr.filter((el) => {
-    //     return
-    //   })
-    // }
-
     return arr.filter((el) => {
       if (thirdfil === 'all') {
         return el[secfil];
       } else {
-        // console.log('el INSIDE FILTERBYSUB-->', el);
         return el[secfil] === thirdfil;
       }
     });
   };
 
   const filteredShit = (arr, filter) => {
-    // console.log('INSIDE FILTEREDSHIT-->');
-    // console.log('arr-->', arr);
-    // console.log('filter-->', filter);
-
     const notUniq = arr.map((el) => {
-      // console.log('el-->', el);
-      // console.log('el[filter]-->', el[filter]);
       return el[filter] + '';
     });
     const res = [...new Set(notUniq)];
     res.push('all');
     return res;
-    // return [...new Set(notUniq)];
   };
 
   const setDefaultSubFilter = (el) => {
-    console.log('INSIDE SETDEFAULTSUBFILTER-->');
-    // console.log('secondFilter-->', secondFilter);
-    // console.log('filteredShit(entries, secondFilter)-->', filteredShit(entries, secondFilter));
-    // console.log('(filteredShit(entries, secondFilter))[0]-->', (filteredShit(entries, secondFilter))[0]);
     setThirdFilter((filteredShit(entries, el))[0]);
-    console.log('thirdFilter-->', thirdFilter);
-
   };
 
   const pickers = (
@@ -190,7 +268,6 @@ const Analysis = ({ entries }) => {
             selectedValue={secondFilter}
             onValueChange={(el) => {
               setSecondFilter(el);
-              console.log('el INSIDE 2 FILTER-->', el);
               setDefaultSubFilter(el);
             }}>
             {getLabels(entries[0]).map((el, i) => {
@@ -220,10 +297,20 @@ const Analysis = ({ entries }) => {
             }
           </Picker>
 
+          <Text>Selected which type of Graph</Text>
+          <Picker
+            selectedValue={selectedChart}
+            onValueChange={(el) => setSelectedChart(el)}>
+            {chartTypes.map((el, i) => {
+              return <Picker.Item key={i} label={el} value={el} />;
+            })
+            }
+          </Picker>
+
           <Button
             onPress={(e) => {
-              handleOutput(entries, firstFilter, secondFilter, thirdFilter, selectedAnalysys);
               setAnalysis(true);
+              handleOutput(entries, firstFilter, secondFilter, thirdFilter, selectedAnalysys, selectedChart);
             }}
             title='Analyse'
           />
@@ -232,14 +319,26 @@ const Analysis = ({ entries }) => {
     </>
   );
 
+  let renderEverything;
+  if (!chartShow) {
+    if (!analysis) {
+      renderEverything = pickers;
+    } else {
+      renderEverything = renderAnalysisResult(firstFilter, secondFilter, thirdFilter, selectedAnalysys, result);
+    }
+  } else {
+    renderEverything = showPieChart(entries);
+  }
+
   return (
     <>
-      {!analysis ? pickers : renderAnalysisResult(firstFilter, selectedAnalysys, result)}
+      {renderEverything}
     </>
   );
 };
 
 import { StyleSheet } from 'react-native';
+import { render } from 'react-dom';
 
 const styles = StyleSheet.create({
   container: {
